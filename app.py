42b0eb278a75d7bb5d5befb979aa9fb623ca896b
ivanays <ivanays@yandex.ru>
2024-08-23 21:05:51 +0300

    edit ok ok

--- app.py
+++ app.py
@@ -8,28 +8,21 @@ try:
                                   port="5432",
                                   database="ivanayskiy")
 
-    cursor_max = connection.cursor()
-    cursor_min = connection.cursor()
-    postgreSQL_select_Query_max = "SELECT MAX(age) FROM test_table WHERE LENGTH(name) < 6"
-    postgreSQL_select_Query_min = "SELECT MIN(age) FROM test_table WHERE LENGTH(name) < 6"
+    cursor = connection.cursor()
+    postgreSQL_select_Query = "SELECT MAX(age), MIN(age) FROM test_table WHERE LENGTH(name) < 6"
 
-    cursor_max.execute(postgreSQL_select_Query_max)
-    cursor_min.execute(postgreSQL_select_Query_min)
+    cursor.execute(postgreSQL_select_Query)
    
-    mobile_records_max = cursor_max.fetchall()
-    mobile_records_min = cursor_min.fetchall()
+    mobile_records = cursor.fetchall()
  
-    for row_max in mobile_records_max:
-        print("Максимальный возраст для людей, длина имён которых меньше 6 символов равен: ", row_max[0], )
-
-    for row_min in mobile_records_min:
-        print("Минимальный возраст для людей, длина имён которых меньше 6 символов равен: ", row_min[0], )
+    for row in mobile_records:
+        print("Максимальный возраст для людей, длина имён которых меньше 6 символов равен: ", row[0], )
+        print("Минимальный возраст для людей, длина имён которых меньше 6 символов равен: ", row[1], )
 
 except (Exception, Error) as error:
     print("Ошибка при работе с PostgreSQL", error)
 finally:
     if connection:
-        cursor_max.close()
-        cursor_min.close()
+        cursor.close()
         connection.close()
         print("Соединение с PostgreSQL закрыто")
