526278f2cb6bf7dd704899a02715661b3009212d
ivanays <ivanays@yandex.ru>
2024-08-23 19:51:22 +0300

    edit ok

--- app.py
+++ app.py
@@ -1,32 +1,35 @@
-import pandas as pd
 import psycopg2
-from sqlalchemy import create_engine
+from psycopg2 import Error
 
-def process_data(engine, num):
-    conn = engine.connect()
+try:
+    connection = psycopg2.connect(user="postgres",
+                                  password="password",
+                                  host="db",
+                                  port="5432",
+                                  database="ivanayskiy")
 
-    max = pd.read_sql('SELECT MAX(age) FROM test_table WHERE LENGTH(name) < 6', conn)
-    min = pd.read_sql('SELECT MIN(age) FROM test_table WHERE LENGTH(name) < 6', conn)
+    cursor_max = connection.cursor()
+    cursor_min = connection.cursor()
+    postgreSQL_select_Query_max = "SELECT MAX(age) FROM test_table WHERE LENGTH(name) < 6"
+    postgreSQL_select_Query_min = "SELECT MIN(age) FROM test_table WHERE LENGTH(name) < 6"
 
-    if num == 1:
-        return max
-    elif num == 2:
-        return min
+    cursor_max.execute(postgreSQL_select_Query_max)
+    cursor_min.execute(postgreSQL_select_Query_min)
+   
+    mobile_records_max = cursor_max.fetchall()
+    mobile_records_min = cursor_min.fetchall()
+ 
+    for row_max in mobile_records_max:
+        print("Максимальный возраст для людей, длина имён которых меньше 6 символов равен: ", row_max[0], )
 
+    for row_min in mobile_records_min:
+        print("Минимальный возраст для людей, длина имён которых меньше 6 символов равен: ", row_min[0], )
 
-if __name__ == "__main__":
-    db_user = 'postgres'
-    db_password = 'password'
-    db_host = 'db'
-    db_port = '5432'
-    db_name = 'ivanayskiy'
-
-    engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
-
-    result_max = process_data(engine,1)
-    result_min = process_data(engine,2)
-
-    print("Максимальный возраст для людей, длина имён которых меньше 6 символов равен: ")
-    print(result_max)
-    print("Минимальный возраст для людей, длина имён которых меньше 6 символов равен: ")
-    print(result_min)
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+finally:
+    if connection:
+        cursor_max.close()
+        cursor_min.close()
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
